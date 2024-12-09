ac94939d8dab20fbe79bda186a54f15d66430c33
WANGZHENZHEN <1092107556@qq.com>
2024-12-09 21:07:05 +0800

    20241209 生成wpt动态图结构原始数据

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+
 import itertools
+import pandas as pd
 
 from graph_modeling import GraphModeling
 from graph_data import transmitter_nodes, receiver_nodes, transmitter_edges, receiver_edges, coupled_edge
 
 
-frequency = 50
+frequency = 85000
 
 
 def create_transmitter_graph(state):
@@ -167,19 +169,68 @@ def create_normal_wpt_graph():
             print(f"  {key}: {value}")
 
     # 绘制总的图
-    for key, item in current_states.items():
-        state = [value for key, value in item.items()]
+    # for key, item in current_states.items():
+    #     state = [value for key, value in item.items()]
+    #
+    #     gm = GraphModeling(
+    #         type="all",
+    #         state=state,
+    #         nodes=all_nodes["Transmitter"][state[0]] + all_nodes["Receiver"][state[1]],
+    #         edges=transmitter_edges[state[0]] + receiver_edges[state[1]] + coupled_edge
+    #     )
+    #     gm.create_graph()
+    #     gm.plot_wpt_graph()
 
-        gm = GraphModeling(
-            type="all",
-            state=state,
-            nodes=all_nodes["Transmitter"][state[0]] + all_nodes["Receiver"][state[1]],
-            edges=transmitter_edges[state[0]] + receiver_edges[state[1]] + coupled_edge
-        )
-        gm.create_graph()
-        gm.plot_wpt_graph()
+    return all_nodes, current_states
+
+
+# 定义函数根据规则生成发射端图结构
+def determine_transmitter_graph(row):
+    S1, S2, S3, S4, Ip = row['S1'], row['S2'], row['S3'], row['S4'], row['transmitter_ac_Ip']
+    if S1 == 1 and S4 == 1:
+        return 'S1S4'
+    elif S2 == 1 and S3 == 1:
+        return 'S2S3'
+    elif S1 == 1 and S3 == 1 and Ip > 0:
+        return 'S1S3_freewheeling'
+    elif S1 == 1 and S3 == 1 and Ip < 0:
+        return 'S1S3_commutation'
+    elif S2 == 1 and S4 == 1 and Ip < 0:
+        return 'S2S4_freewheeling'
+    elif S2 == 1 and S4 == 1 and Ip > 0:
+        return 'S2S4_commutation'
+    return 'Unknown'
+
+
+# 根据新的规则更新接收端图结构生成函数
+def determine_receiver_graph(row):
+    D1, D2, D3, D4 = row['D1'], row['D2'], row['D3'], row['D4']
+    if D1 == 1 and D4 == 1:
+        return 'D1D4'
+    elif D2 == 1 and D3 == 1:
+        return 'D2D3'
+    return 'Unknown'
+
+
+def main(df):
+    # 应用规则生成图结构数据
+    df['transmitter_graph'] = df.apply(determine_transmitter_graph, axis=1)
+    df['receiver_graph'] = df.apply(determine_receiver_graph, axis=1)
+    df['wpt_graph'] = df['transmitter_graph'] + '+' + df['receiver_graph']
+
+    # 仅保留相关列
+    result_df = df[['transmitter_graph', 'receiver_graph', 'wpt_graph']]
+
+    # 保存生成的数据到新的CSV文件
+    output_path = 'wpt_graph_structure.csv'
+    result_df.to_csv(output_path, index=False)
+
+    # 打印保存成功的消息到控制台
+    print(f"文件已成功保存为: {output_path}")
 
 
 if __name__ == '__main__':
-    create_normal_wpt_graph()
+    file_path = 'all_simulation_results.csv'
+    data = pd.read_csv(file_path)
+    main(df=data)
 
